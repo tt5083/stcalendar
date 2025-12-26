140e4d1a7badbf8276ea92d638885422835226d1
Peter <pcdiyamd@hotmail.com>
2025-12-26 11:31:54 +0800

    初期將撈「整個月事件」輸出到日曆中

--- calendar_v3.php
+++ calendar_v3.php
@@ -1,6 +1,34 @@
 <?php
 require_once __DIR__ . "/connection/default.php";
 require 'sys_header_v2.php';
+/* === ① 撈本月所有事件（一次就好） === */
+$startDate = sprintf('%04d-%02d-01', $year, $month);
+$endDate   = date('Y-m-t', strtotime($startDate));
+
+$sql = "
+    SELECT
+        ca_theme,
+        ca_even_str_time,
+        event_date
+    FROM events
+    WHERE event_date BETWEEN :start AND :end
+    ORDER BY ca_even_str_time
+";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute([
+    ':start' => $startDate,
+    ':end'   => $endDate
+]);
+
+$events = $stmt->fetchAll();
+
+/* === ② 整理成「日期 => 事件陣列」 === */
+$eventMap = [];
+
+foreach ($events as $event) {
+    $eventMap[$event['event_date']][] = $event;
+}
 ?>
 
 <body>
@@ -10,11 +38,6 @@ require 'sys_header_v2.php';
     <br>
     <?php
     echo "目前月份：" . $month;
-    ?>
-    <br>
-    <br>
-    <?php
-    echo $daysInMonth;
     ?>
     <div style="display: flex;" class="big-table">
         <div style="text-align: left">
@@ -61,7 +84,24 @@ require 'sys_header_v2.php';
                     echo "<td class='{$class}'>{$day}</td></tr><tr>";
                 } else {
                     $class = (date('Y-m-d', mktime(0, 0, 0, $month, $day, $year)) == $today) ? "today" : "";
-                    echo "<td class='{$class}'>{$day}</td>";
+                    //因應一次查找一整個月做修改
+                    /* echo "<td class='{$class}'>{$day}</td>"; */
+                    $date = sprintf('%04d-%02d-%02d', $year, $month, $day);
+                    $class = ($date === $today) ? "today" : "";
+
+                    echo "<td class='{$class}'>";
+                    echo "<div class='day-num'>{$day}</div>";
+
+                    /* === 顯示事件 === */
+                    if (!empty($eventMap[$date])) {
+                        foreach ($eventMap[$date] as $event) {
+                            $time = date('H:i', strtotime($event['ca_even_str_time']));
+                            $title = htmlspecialchars($event['ca_theme']);
+                            echo "<div class='event-item'>{$time} {$title}</div>";
+                        }
+                    }
+
+                    echo "</td>";
                 }
             }
             ?>
